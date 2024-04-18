64e8ab13504fb0ecc46a1b5f090598f9363e1b8e
DawidRozewski <dawid.rrozewski@gmail.com>
2024-04-18 16:40:38 +0200

    SAN-019

--- src/main/java/com/dawidrozewski/sandbox/cart/controller/CartItemController.java
+++ src/main/java/com/dawidrozewski/sandbox/cart/controller/CartItemController.java
@@ -2,10 +2,7 @@ package com.dawidrozewski.sandbox.cart.controller;
 
 import com.dawidrozewski.sandbox.cart.service.CartItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,4 +15,10 @@ public class CartItemController {
     public void deleteCartItem(@PathVariable Long id) {
         cartItemService.delete(id);
     }
+
+    @GetMapping("/count/{cartId}")
+    public Long countItemInCart(@PathVariable Long cartId) {
+        return cartItemService.countItemInCart(cartId);
+    }
+
 }
--- src/main/java/com/dawidrozewski/sandbox/cart/repository/CartItemRepository.java
+++ src/main/java/com/dawidrozewski/sandbox/cart/repository/CartItemRepository.java
@@ -4,4 +4,5 @@ import com.dawidrozewski.sandbox.cart.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Long countByCartId(Long cartId);
 }
