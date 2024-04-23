9849b267538a1b78aeb141e11fcae75637c521ec
DawidRozewski <dawid.rrozewski@gmail.com>
2024-04-23 16:22:08 +0200

    SAN-036

--- src/main/java/com/dawidrozewski/sandbox/admin/order/service/AdminOrderService.java
+++ src/main/java/com/dawidrozewski/sandbox/admin/order/service/AdminOrderService.java
@@ -52,6 +52,9 @@ public class AdminOrderService {
     private void processOrderStatusChange(AdminOrder adminOrder, Map<String, String> values) {
         AdminOrderStatus oldStatus = adminOrder.getOrderStatus();
         AdminOrderStatus newStatus = AdminOrderStatus.valueOf(values.get("orderStatus"));
+        if (oldStatus == newStatus) {
+            return;
+        }
         adminOrder.setOrderStatus(newStatus);
         logStatusChange(adminOrder.getId(), oldStatus, newStatus);
         emailNotificationForStatusChange.sendEmailNotification(newStatus, adminOrder);
