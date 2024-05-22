97402d6fb582b61d848268de4ae7b2b7b6acd588
Dawid Rozewski <dawid_rozewski@epam.com>
2024-05-22 13:12:34 +0200

    SAN-061

--- src/test/java/com/dawidrozewski/sandbox/AbstractConfiguredTest.java
+++ src/test/java/com/dawidrozewski/sandbox/AbstractConfiguredTest.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,4 +21,21 @@ public abstract class AbstractConfiguredTest {
 
     @Autowired
     protected MockMvc mockMvc;
+
+    @Container
+    public static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:10.5.8")
+            .withDatabaseName("integration-tests-db")
+            .withUsername("username")
+            .withPassword("password");
+
+    static {
+        mariaDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void mariaDBProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mariaDBContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mariaDBContainer::getUsername);
+        registry.add("spring.datasource.password", mariaDBContainer::getPassword);
+    }
 }
